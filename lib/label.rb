999dd52f32f59897f69d4b70006a8be42b72e597
Shisui <37116322+Shisui6@users.noreply.github.com>
2023-02-14 11:05:40 +0100

    Create label class

---
+++ lib/label.rb
@@ -0,0 +1,15 @@
+require 'securerandom'
+
+class Label
+  def initialize(title, color)
+    @id = SecureRandom.uuid
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.label = self
+  end
+end
