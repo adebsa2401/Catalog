710d126496f380f9d14d2fcbfaf0d0b2ccae2a31
z4cope <63551733+z4cope@users.noreply.github.com>
2023-02-15 14:10:31 +0200

    Fix linters errors

--- lib/music.rb
+++ lib/music.rb
@@ -1,18 +1,16 @@
-require_relative "./item"
+require_relative './item'
 
 class MusicAlbum < Item
   attr_accessor :publish_date, :on_spotify
 
-  def initialize(publish_date, on_spotify)
+  def initialize(publish_date, _on_spotify)
     super(publish_date)
     @on_spotify = false
   end
 
   def can_be_archived?
-    if super && on_spotify
-      return true
-    else
-      return false
-    end
+    return true if super && on_spotify
+
+    false
   end
 end
