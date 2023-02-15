7b50ff4442a44d568144d58a1af8bb167451d914
Shisui <37116322+Shisui6@users.noreply.github.com>
2023-02-15 10:05:02 +0100

    Create schema for label and book tables

---
+++ lib/schema/schema.sql
@@ -0,0 +1,18 @@
+-- Books table
+CREATE TABLE books (
+    id                  SERIAL PRIMARY KEY,
+    publish_date        DATE,
+    archived            BOOLEAN,
+    genre_id            INT REFERENCES genre(id)
+    author_id           INT REFERENCES author(id)
+    source_id           INT REFERENCES source(id)
+    label_id            INT REFERENCES label(id)
+);
+
+-- Label table
+CREATE TABLE label (
+    id                  SERIAL PRIMARY KEY,
+    title               VARCHAR(100),
+    color               VARCHAR(100),
+    items               JSON
+);
