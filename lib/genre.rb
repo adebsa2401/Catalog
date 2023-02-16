c8a672a7567cb8f25f083c55f61aee8d29a3b0d5
z4cope <63551733+z4cope@users.noreply.github.com>
2023-02-16 11:33:03 +0200

    Add genre class

---
+++ lib/genre.rb
@@ -0,0 +1,14 @@
+require_relative './item'
+
+class Genre < Item
+  attr_accessor :name
+
+  def initialize(name)
+    super()
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = []
+  end
+
+  def add_item; end
+end
