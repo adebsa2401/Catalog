ebcf8553a6a2f0bdbc021af97e40568af6c17d57
Ben Sales <adebsa2401@gmail.com>
2023-02-16 13:41:59 +0100

    feat: Add games and authors menu

--- lib/app.rb
+++ lib/app.rb
@@ -1,4 +1,5 @@
 require './book'
+require './game'
 require 'json'
 
 class App
@@ -17,6 +18,14 @@ class App
     end
   end
 
+  def list_games
+    if @games.empty?
+      puts "No games saved\n"
+    else
+      @games.each { |g| puts "Published: #{g.publish_date}, Last played: #{g.last_played_at}, Multiplayer: #{g.multiplayer ? 'Yes' : 'No' }" }
+    end
+  end
+
   def list_labels
     if @labels.empty?
       puts "No labels saved\n"
@@ -25,10 +34,24 @@ class App
     end
   end
 
+  def list_authors
+    if @authors.empty?
+      puts "No authors saved\n"
+    else
+      @authors.each { |a| puts "First name: #{a.first_name}, Last name: #{a.last_name}" }
+    end
+  end
+
   def add_book(publish_date, publisher, cover_state)
     book = Book.new(publish_date, publisher, cover_state)
     @books << book
     puts "Book added successfully\n"
+  end
+
+  def add_game(publish_date, multiplayer, last_played_at)
+    game = Game.new(publish_date, multiplayer, last_played_at)
+    @games << game
+    puts "Game added successfully\n"
   end
 
   def handle_add_book
@@ -47,6 +70,30 @@ class App
       add_book(publish_date, publisher, 'good')
     when 'n'
       add_book(publish_date, publisher, 'bad')
+    else
+      puts "Invalid input, please try again\n"
+    end
+  end
+
+  def handle_add_game
+    print 'What year was the game published:'
+    publish_date = gets.chomp
+    if publish_date.to_i.zero?
+      puts "\nPlease enter a valid year E.G 2000"
+      return
+    end
+
+    print 'Multiplayer [Y/N]:'
+    multiplayer = gets.chomp.downcase
+
+    print 'When is the game played the last time?:'
+    last_played_at = gets.chomp
+
+    case multiplayer
+    when 'y'
+      add_game(publish_date, true, last_played_at)
+    when 'n'
+      add_game(publish_date, false, last_played_at)
     else
       puts "Invalid input, please try again\n"
     end
