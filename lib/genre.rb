f9f6a3ded818c709527fb8a30c4bbcbd9d2434d1
z4cope <63551733+z4cope@users.noreply.github.com>
2023-02-16 17:36:35 +0200

    Forfeit Genre class from Item class

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,14 +1,13 @@
-require_relative './item'
-
-class Genre < Item
-  attr_accessor :name
+class Genre
+  attr_accessor :name, :items
 
   def initialize(name)
-    super()
     @id = Random.rand(1..1000)
     @name = name
     @items = []
   end
 
-  def add_item; end
+  def add_item(item)
+    @items << item
+  end
 end
